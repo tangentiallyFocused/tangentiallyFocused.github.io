d3b02235c896e96d5417ca9d64f9b0de1a36b004
tangentiallyFocused <lb117@wellesley.edu>
2025-04-12 18:53:40 -0400

    adjusted header-footer for index

--- src/headerTestJS.js
+++ src/headerTestJS.js
@@ -2,8 +2,14 @@ let header_html = "";
 header_html += '<img id="icon">';
 header_html += '<div class="spacer"></div>';
 header_html += '<div id="my_name">';
-    header_html += '<div id="name_a"></div>';
-    header_html += '<div id="name_b"></div>';
+    header_html += '<div id="name_ab">';
+        header_html += '<div id="name_a" class="name_part"></div>';
+        header_html += '<div id="name_b" class="name_part"></div>';
+    header_html += '</div>';
+    header_html += '<div id="name_cd">';
+        header_html += '<div id="name_c" class="name_part"></div>';
+        header_html += '<div id="name_d" class="name_part"></div>';
+    header_html += '</div>';
 header_html += '</div>';
 header_html += '<img id="open_menu" class="menu_btn">';
 
@@ -16,7 +22,7 @@ document.getElementById("overlay_menu").innerHTML += overlay_html + "\n";
 
 import header from '/src/jsons/header_bar.json' with {type:"json"};
 
-const cycle_ids = ["icon", "name_a", "name_b", "open_menu", "close_menu"];
+const cycle_ids = ["icon", "name_a", "name_b", "name_c", "name_d", "open_menu", "close_menu"];
 
 for(let idNum = 0; idNum < cycle_ids.length; idNum++) {
     let element_id = cycle_ids[idNum];
@@ -104,14 +110,15 @@ let footer_html = "";
 footer_html += '<div id="copyright"></div>';
 footer_html += '<div class="spacer"></div>';
 footer_html += '<div id="footer_socials">';
-    footer_html += '<span id="linkedin"></span> • ';
+    footer_html += "<svg id='linkedin' xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-linkedin' viewBox='0 0 16 16'> <path d='M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z'/> </svg>";
+    footer_html += '<span>•</span>';
     footer_html += '<email id="email"></email>';
 footer_html += '</div>';
 document.getElementById("footer_bar").innerHTML += footer_html;
 
 import footer from '/src/jsons/footer_bar.json' with {type:"json"};
 
-const cycle_fids = ["copyright", "linkedin", "email"];
+const cycle_fids = ["copyright", "email"];
 
 for(let idNum = 0; idNum < cycle_ids.length; idNum++) {
     let element_id = cycle_fids[idNum];
@@ -122,9 +129,6 @@ for(let idNum = 0; idNum < cycle_ids.length; idNum++) {
 
             if(element.nodeName == "DIV") {
                 element.innerText = part.content;
-            } 
-            else if (element.nodeName == "SPAN") {
-                element.innerHTML = part.content;
             } 
             else if (element.nodeName == "EMAIL") {
                 element.innerText = part.content;

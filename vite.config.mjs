ea24ee3ecae02b18db78ac7709ae73a70da5d514
tangentiallyFocused <lb117@wellesley.edu>
2025-12-02 01:22:39 -0800

    init

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,5 +5,12 @@ export default ({
   //   outDir: '../',
     // assetsDir: 'public',
     // assetsInlineLimit: 22*1024*1024
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   }
 })
